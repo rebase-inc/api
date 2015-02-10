a68a775cf1ba852be328bf60e24690cfde1999ab
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2015-02-10 01:49:49 -0800

    Fix variable naming in bid limit model tests

--- alveare/tests/unit/models/test_bid_limit.py
+++ alveare/tests/unit/models/test_bid_limit.py
@@ -6,22 +6,22 @@ class TestBidLimitModel(AlveareModelTestCase):
     model = models.BidLimit
 
     def test_create(self):
-        new_bank_account = self.create_model(self.model, 10)
-        self.assertEqual(new_bank_account.price, 10)
+        new_bid_limit = self.create_model(self.model, 10)
+        self.assertEqual(new_bid_limit.price, 10)
 
     def test_delete(self):
-        new_bank_account = self.create_model(self.model, 20)
-        self.delete_instance(self.model, new_bank_account)
+        new_bid_limit = self.create_model(self.model, 20)
+        self.delete_instance(self.model, new_bid_limit)
 
     def test_update(self):
-        new_bank_account = self.create_model(self.model, 30)
-        self.assertEqual(new_bank_account.price, 30)
+        new_bid_limit = self.create_model(self.model, 30)
+        self.assertEqual(new_bid_limit.price, 30)
 
-        new_bank_account.price = 40
+        new_bid_limit.price = 40
         self.db.session.commit()
 
-        modified_bank_account = self.model.query.get(new_bank_account.id)
-        self.assertEqual(modified_bank_account.price, 40)
+        modified_bid_limit = self.model.query.get(new_bid_limit.id)
+        self.assertEqual(modified_bid_limit.price, 40)
 
     def test_bad_create(self):
         with self.assertRaises(ValueError):
