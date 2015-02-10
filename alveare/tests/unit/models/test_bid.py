b3e899d8f384d54835637c782ca34469fb77d364
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2015-02-10 01:26:56 -0800

    Add test for bid model

---
+++ alveare/tests/unit/models/test_bid.py
@@ -0,0 +1,29 @@
+import unittest
+
+from . import AlveareModelTestCase
+
+from alveare import models
+
+class TestBidModel(AlveareModelTestCase):
+    model = models.Bid
+
+    def test_create(self):
+        new_bid = self.create_model(self.model)
+
+    def test_delete(self):
+        new_bid = self.create_model(self.model)
+        self.delete_instance(self.model, new_bid)
+
+    @unittest.skip('Bid model doesnt have any updatable fields yet')
+    def test_update(self):
+        new_bid = self.create_model(self.model)
+
+    @unittest.skip('Bid model doesnt have any creation fields yet')
+    def test_bad_create(self):
+        with self.assertRaises(ValueError):
+            self.create_model(self.model, 'foo', 2)
+        with self.assertRaises(ValueError):
+            self.create_model(self.model, 1234, 2)
+        with self.assertRaises(ValueError):
+            self.create_model(self.model, 123456789, 'foo')
+
