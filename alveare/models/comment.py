e9952a2d00b37c30fc2f3cafff6a5de1483e1a43
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2015-02-10 01:47:12 -0800

    Add content field to comment model

--- alveare/models/comment.py
+++ alveare/models/comment.py
@@ -2,7 +2,11 @@
 from alveare.common.database import DB
 
 class Comment(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
+    id =      DB.Column(DB.Integer, primary_key=True)
+    content = DB.Column(DB.String,  nullable=False)
+
+    def __init__(self, content):
+        self.content = content
 
     def __repr__(self):
         return '<Comment[id:{}]>'.format(self.id)
