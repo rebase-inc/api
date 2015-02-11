7900fad6f19b74629ca17a9393defdf87277e593
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2015-02-11 14:50:07 -0800

    Added database switch

--- alveare/__init__.py
+++ alveare/__init__.py
@@ -1,26 +1,28 @@
 import sys
 
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 sys.dont_write_bytecode = True
 
-from sqlalchemy.engine import Engine
-from sqlalchemy import event
-
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
-
-def create_app(database, config_filename = 'config'):
+def create_app(sqlalchemy_object, database_type = 'sqlite', config_filename = 'config'):
     """ Create our app using the Flask factory pattern """
     app = Flask(__name__)
-    settings = dict(username='postgres', password='', host='localhost', port='5432', dbname='postgres')
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{username}:{password}@{host}:{port}/{dbname}'.format(**settings)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    if database_type == 'sqlite':
+        @event.listens_for(Engine, "connect")
+        def set_sqlite_pragma(dbapi_connection, connection_record):
+            cursor = dbapi_connection.cursor()
+            cursor.execute("PRAGMA foreign_keys=ON")
+            cursor.close()
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    elif database_type == 'postgres':
+        settings = dict(username='postgres', password='', host='localhost', port='5432', dbname='postgres')
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{username}:{password}@{host}:{port}/{dbname}'.format(**settings)
+    else:
+        raise Exception('invalid database type!')
     #app.config.from_object(config_filename)
-    database.init_app(app)
+    sqlalchemy_object.init_app(app)
     return app
 
