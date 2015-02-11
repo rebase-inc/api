16f64507b1f17715a28f3df63d63fd317b07053f
Raphael Goyran <rapha.open.source@gmail.com>
2015-02-11 14:29:00 -0800

    Add relationships for Auction, TicketSet, BidLimit, TicketSnapshot, Ticket, plus tests

--- alveare/models/ticket_snapshot.py
+++ alveare/models/ticket_snapshot.py
@@ -1,12 +1,18 @@
 
 from alveare.common.database import DB
+from alveare.models.ticket import Ticket
+from datetime import datetime
 
 class TicketSnapshot(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     date = DB.Column(DB.DateTime, nullable=False)
+    bid_limit_id = DB.Column(DB.Integer, DB.ForeignKey('bid_limit.id'))
+    ticket_id = DB.Column(DB.Integer, DB.ForeignKey('ticket.id'))
+    ticket = DB.relationship(Ticket)
 
-    def __init__(self, date):
-        self.date = date
+    def __init__(self, ticket):
+        self.date =  datetime.now()
+        self.ticket = ticket
 
     def __repr__(self):
 	    return '<TicketSnapshot[id:{}] date: {}>'.format(self.id, self.date)
--- alveare/tests/unit/models/test_auction.py
+++ alveare/tests/unit/models/test_auction.py
@@ -4,27 +4,46 @@ import datetime
 from sqlalchemy.exc import StatementError
 
 from . import AlveareModelTestCase
-from alveare import models
+from alveare.models import Auction, Ticket, TicketSnapshot, TicketSet
 
 class TestAuctionModel(AlveareModelTestCase):
-    model = models.Auction
+
+    def setUp(self):
+        self.ticket_prices = [ (Ticket('Foo','Bar'), 111), (Ticket('Joe', 'Blow'), 222) ]
+        super().setUp()
 
     def test_create_auction(self):
         current_date = datetime.datetime.today()
-        new_auction = self.create_model(self.model, 1000, current_date, 1)
+        new_auction = self.create_model(Auction, self.ticket_prices, 1000, current_date, 1)
 
         self.assertEqual(new_auction.duration, 1000)
         self.assertEqual(new_auction.finish_work_by, current_date)
         self.assertEqual(new_auction.redundancy, 1)
+        self.assertNotEqual(new_auction.ticket_set, None)
+        self.assertEqual(new_auction.ticket_set.auction_id, new_auction.id)
+        self.assertEqual(len(new_auction.ticket_set.bid_limits), 2)
+        self.assertEqual(new_auction.ticket_set.bid_limits[0].snapshot.ticket.title, 'Foo')
+        self.assertEqual(new_auction.ticket_set.bid_limits[1].snapshot.ticket.title, 'Joe')
+
+        snapshots = TicketSnapshot.query.all()
+        self.assertEqual(snapshots[0].ticket.title, 'Foo')
+        self.assertEqual(snapshots[1].ticket.title, 'Joe')
+
+        tickets = Ticket.query.all()
+        self.assertEqual(tickets[0].title, 'Foo')
+        self.assertEqual(tickets[1].title, 'Joe')
 
     def test_delete_auction(self):
         current_date = datetime.datetime.today()
-        new_auction = self.create_model(self.model, 2000, current_date, 1)
-        self.delete_instance(self.model, new_auction)
+        new_auction = self.create_model(Auction, self.ticket_prices, 2000, current_date, 1)
+        self.delete_instance(Auction, new_auction)
+
+        # enable this when postgresql is installed
+        #self.assertEqual( TicketSet.query.all(), [])
 
     def test_update_auction(self):
         current_date = datetime.datetime.today()
-        new_auction = self.create_model(self.model, 3000, current_date, 1)
+        new_auction = self.create_model(Auction, self.ticket_prices, 3000, current_date, 1)
 
         self.assertEqual(new_auction.duration, 3000)
         self.assertEqual(new_auction.finish_work_by, current_date)
@@ -37,15 +56,15 @@ class TestAuctionModel(AlveareModelTestCase):
 
         self.db.session.commit()
 
-        modified_auction = self.model.query.get(new_auction.id)
+        modified_auction = Auction.query.get(new_auction.id)
         self.assertEqual(modified_auction.duration, 4000)
         self.assertEqual(modified_auction.finish_work_by, tomorrows_date)
         self.assertEqual(modified_auction.redundancy, 2)
 
     def test_bad_create(self):
         with self.assertRaises(ValueError):
-            self.create_model(self.model, 'foo', datetime.datetime.today(), 1)
+            self.create_model(Auction, self.ticket_prices, 'foo', datetime.datetime.today(), 1)
         with self.assertRaises(StatementError):
-            self.create_model(self.model, 1, 'foo', 1)
+            self.create_model(Auction, self.ticket_prices, 1, 'foo', 1)
         with self.assertRaises(ValueError):
-            self.create_model(self.model, 1, datetime.datetime.today(), 'foo')
+            self.create_model(Auction, self.ticket_prices, 1, datetime.datetime.today(), 'foo')
