51b9f71170dc355800b1c44704bd55be30747926
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2015-02-12 16:20:00 -0800

    More resource creation abstraction

--- alveare/tests/unit/models/__init__.py
+++ alveare/tests/unit/models/__init__.py
@@ -18,10 +18,7 @@ class AlveareModelTestCase(AlveareTestCase):
         self.assertEqual(model.query.get(instance.id), None)
 
     def create_work(self, title, description, work_price=100, review=None, debit=None, credit=None, mediation_rounds=0):
-        ticket = models.Ticket(title, description)
-        ticket_snapshot = models.TicketSnapshot(ticket)
-        bid = models.Bid()
-        work_offer = models.WorkOffer(bid, ticket_snapshot, work_price)
+        work_offer = self.create_work_offer(title, description, work_price)
         work = self.create_model(self.model, work_offer)
 
         if review:
@@ -36,3 +33,30 @@ class AlveareModelTestCase(AlveareTestCase):
         self.db.session.add(work)
         self.db.session.commit()
         return work
+
+    def create_arbitration():
+        work = self.create_work('dontcare', 'dontcare', 666, mediation_rounds=1)
+        return work.mediation_rounds.one().arbitration
+
+    def create_mediation():
+        work = self.create_work('dontcare', 'dontcare', 666, mediation_rounds=1)
+        return work.mediation_rounds.one()
+
+    def create_review(rating, comment=None):
+        work = self.create_work('dontcare', 'dontcare', 666, rating)
+        if comment:
+            _ = models.Comment(work.review, 'Hello')
+        return work.review
+
+    def create_debit_and_credit(debit_price, credit_price):
+        work = self.create_work('dontcare', 'dontcare', 666, debit=debit_price, credit=credit_price)
+        return (work.debit, work.credit)
+
+    def create_work_offer(self, title, description, work_price):
+        ticket = models.Ticket(title, description)
+        ticket_snapshot = models.TicketSnapshot(ticket)
+        bid = models.Bid()
+        work_offer = models.WorkOffer(bid, ticket_snapshot, work_price)
+        self.db.session.add(work_offer)
+        self.db.session.commit()
+        return work_offer
--- alveare/tests/unit/models/test_review.py
+++ alveare/tests/unit/models/test_review.py
@@ -5,27 +5,15 @@ from alveare import models
 class TestReviewModel(AlveareModelTestCase):
 
     def test_create(self):
-        ticket_snap = self.create_model(models.TicketSnapshot, models.Ticket('baz', 'qux'))
-        bid = models.Bid()
-        work_offer = models.WorkOffer(bid, ticket_snap, 100)
-        work = self.create_model(models.Work, work_offer)
-        review = self.create_model(models.Review, work, 1)
-        _ = models.Comment(review, 'Hello')
+        review = self.create_review(3, comment = 'Hello')
 
-        self.db.session.commit()
         found_review = models.Review.query.get(review.id)
-
-        self.assertEqual(found_review.work.id, work.id)
-        self.assertEqual(found_review.rating, 1)
-        comment = found_review.comments.one()
-        self.assertEqual(comment.content, 'Hello')
+        self.assertEqual(found_review.work.id, review.work.id)
+        self.assertEqual(found_review.rating, 3)
+        self.assertEqual(found_review.comments.one().content, 'Hello')
 
     def test_delete(self):
-        ticket_snap = self.create_model(models.TicketSnapshot, models.Ticket('baz', 'qux'))
-        bid = models.Bid()
-        work_offer = models.WorkOffer(bid, ticket_snap, 100)
-        work = self.create_model(models.Work, work_offer)
-        review = self.create_model(models.Review, work, 2)
+        review = self.create_review(2)
 
         found_review = models.Review.query.get(review.id)
         self.db.session.delete(found_review)
@@ -34,24 +22,16 @@ class TestReviewModel(AlveareModelTestCase):
         self.assertEqual(models.Review.query.get(review.id), None)
 
     def test_delete_comment(self):
-        ticket_snap = self.create_model(models.TicketSnapshot, models.Ticket('baz', 'qux'))
-        bid = models.Bid()
-        work_offer = models.WorkOffer(bid, ticket_snap, 100)
-        work = self.create_model(models.Work, work_offer)
-        review = models.Review(work, 4)
-        comment = self.create_model(models.Comment, review, 'Bye')
-        self.delete_instance(models.Comment, comment)
+        review = self.create_review(4, comment = 'Bye')
+        self.delete_instance(models.Comment, review.comment)
         self.assertNotEqual(models.Review.query.get(review.id), None)
 
     def test_update(self):
-        ticket_snap = self.create_model(models.TicketSnapshot, models.Ticket('baz', 'qux'))
-        bid = models.Bid()
-        work_offer = models.WorkOffer(bid, ticket_snap, 100)
-        work = self.create_model(models.Work, work_offer)
-        review = self.create_model(models.Review, work, 3)
+        review = self.create_review(3)
+
         found_review = models.Review.query.get(review.id)
         self.assertEqual(found_review.rating, 3)
-        self.assertEqual(found_review.work.id, work.id)
+        self.assertEqual(found_review.work.id, review.work.id)
 
         found_review.rating = 4
         self.db.session.commit()
@@ -60,14 +40,10 @@ class TestReviewModel(AlveareModelTestCase):
         self.assertEqual(found_review.rating, 4)
 
     def test_bad_create(self):
-        ticket_snap = self.create_model(models.TicketSnapshot, models.Ticket('baz', 'qux'))
-        bid = models.Bid()
-        work_offer = models.WorkOffer(bid, ticket_snap, 100)
-        work = self.create_model(models.Work, work_offer)
         with self.assertRaises(ValueError):
-            self.create_model(models.Review, work, 'foo')
+            review = self.create_review('foo')
         with self.assertRaises(ValueError):
-            self.create_model(models.Review, work, -1)
+            review = self.create_review(-1)
         with self.assertRaises(ValueError):
-            self.create_model(models.Review, work, 6)
+            review = self.create_review(6)
 
