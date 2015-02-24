473e8529e5a575caf1e1f2497b996328eacacf6e
Raphael Goyran <rapha.open.source@gmail.com>
2015-02-24 15:05:21 -0800

    Fix TicketMatch so they get updated when a JobFit gets deleted

--- alveare/models/ticket_match.py
+++ alveare/models/ticket_match.py
@@ -4,7 +4,7 @@ from alveare.models.job_fit import JobFit
 
 class TicketMatch(DB.Model):
     __table_args__ = (DB.ForeignKeyConstraint(  ['contractor_id',           'auction_id'],
-                                                [JobFit.contractor_id,   JobFit.auction_id]), {})
+                                                [JobFit.contractor_id,   JobFit.auction_id], ondelete='SET NULL'), {})
 
     skill_requirements_id = DB.Column(DB.Integer, DB.ForeignKey('skill_requirements.id', ondelete='CASCADE'), primary_key=True)
     skill_set_id =          DB.Column(DB.Integer, DB.ForeignKey('skill_set.id',          ondelete='CASCADE'), primary_key=True)
@@ -14,7 +14,7 @@ class TicketMatch(DB.Model):
 
     skill_set =             DB.relationship('SkillSet',             backref='ticket_matches', uselist=False)
     skill_requirements =    DB.relationship('SkillRequirements',    backref='ticket_matches', uselist=False)
-    job_fit =               DB.relationship('JobFit', uselist=False)
+    job_fit =               DB.relationship('JobFit', backref='ticket_matches', uselist=False)
 
     def __init__(self, skill_set, skill_requirements, score):
         self.skill_set = skill_set
