e6ba04644115d8a8f6b76e7c6a796fceedb938b7
Raphael Goyran <rapha.open.source@gmail.com>
2015-02-24 21:00:26 -0800

    Add validation to StateMachine with corresponding unit tests

--- alveare/common/state.py
+++ alveare/common/state.py
@@ -9,11 +9,19 @@ MACHINE_SET = MachineSet()
 
 class StateMachine(object):
 
-    def __init__(self, initial_state):
+    def validate_state(self, state):
+        if not state:
+            return # None is a valid state
+        # anything else should be a method of this object
+        state_name = getattr(state, '__name__') # not a method
+        getattr(self, state_name) # not in self object
+
+    def __init__(self, initial_state=None):
         self.event_transitions = {}
         self.queue = []
         self.internal_queue = []
         self._current_state = initial_state
+        self.validate_state(initial_state)
 
         MACHINE_SET.add(self)
 
@@ -30,11 +38,26 @@ class StateMachine(object):
         raise NotImplementedError()
 
     def add_event_transitions(self, event_name, transitions):
+        '''
+        Add all possible state transitions for an event.
+
+        Params:
+        event_name:     name of one event, string
+        transitions:    dictionary of { current_state: new_state }
+
+        Note that 'current_state' and 'new_state' must be methods provided by the 
+        StateMachine object, usually by a class deriving from StateMachine.
+        If not, an AttributeError exception will be raised.
+        '''
         self.event_transitions[event_name] = {}
         for from_state, to_state in transitions.items():
+            self.validate_state(from_state)
+            self.validate_state(to_state)
             self.event_transitions[event_name][from_state] = to_state
 
     def send(self, event, *args, **kwargs):
+        if event not in self.event_transitions.keys():
+            raise ValueError('Unknown event "{}" sent to {}',format(event, str(self)))
         self.queue.append((event, args, kwargs))
 
     def has_events(self):
--- alveare/tests/unit/models/test_state.py
+++ alveare/tests/unit/models/test_state.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import defaultdict
 from alveare.common.state import StateMachine, MACHINE_SET
 
 class TestState(unittest.TestCase):
@@ -31,68 +32,140 @@ class TestState(unittest.TestCase):
         test.assertEqual(time_line, ['pong', 'ping', 'pong', 'ping', 'pong'])
 
 
-        def test_pair(test):
-            sequence_diagram = defaultdict(list)
+    def test_pair(test):
+        sequence_diagram = defaultdict(list)
 
-            class A(StateMachine):
+        class A(StateMachine):
+            '''
+                                         +---------+ 
+             +----------+                | Waiting | 
+             | Waiting  |<-------pong----+ For     | 
+             | For      |                | Pong    | 
+             | Ping     +-------ping---->|         | 
+             |          |                |         | 
+             +----------+                +---------+ 
+            '''
+            def set_state(self, new_state):
+                sequence_diagram[self.name].append(new_state.__name__)
+
+            def wait_for_ping(self):
+                ''' 
+                    Wait for an external client to send a 'ping' event to A
                 '''
-                                             +---------+ 
-                 +----------+                | Waiting | 
-                 | Waiting  |<-------pong----+ For     | 
-                 | For      |                | Pong    | 
-                 | Ping     +-------ping---->|         | 
-                 |          |                |         | 
-                 +----------+                +---------+ 
-                '''
-                def set_state(self, new_state):
-                    sequence_diagram[self.name].append(new_state)
+                pass
 
-                def wait_for_ping(self, b):
-                    ''' 
-                        Wait for an external client to send a 'ping' event to A
-                    '''
-                    pass
+            def wait_for_pong(self, b):
+                b.send('ping', a)
 
-                def wait_for_pong(self, b):
-                    b.send('ping', a)
+            def __init__(self, name):
+                self.name = name
+                StateMachine.__init__(self, self.wait_for_ping)
+                self.add_event_transitions('ping', {
+                    self.wait_for_ping:  self.wait_for_pong
+                })
+                self.add_event_transitions('pong', {
+                    self.wait_for_pong:  self.wait_for_ping
+                })
 
-                def __init__(self, name):
-                    self.name = name
-                    StateMachine.__init__(self, self.wait_for_ping)
-                    self.add_event_transitions('ping', {
-                        self.ping:  self.wait_for_pong
-                    })
-                    self.add_event_transitions('pong', {
-                        self.wait_for_pong:  self.wait_for_ping
-                    })
+        class B(StateMachine):
+            '''
+                     +---------+
+                     |         |
+                  +--+-----+   |
+                  |  Wait  |   |
+                  |  For   <---+
+                  |  Ping  |    
+                  +--------+    
+            '''
+            def set_state(self, new_state):
+                sequence_diagram[self.name].append(new_state.__name__)
 
-            class B(StateMachine):
-                '''
-                         +---------+
-                         |         |
-                      +--+-----+   |
-                      |  Wait  |   |
-                      |  For   <---+
-                      |  Ping  |    
-                      +--------+    
-                '''
-                def set_state(self, new_state):
-                    sequence_diagram[self.name].append(new_state)
+            def wait_for_ping(self, a):
+                a.send('pong')
 
-                def wait_for_ping(self, a):
-                    a.send('pong')
+            def __init__(self, name):
+                self.name = name
+                StateMachine.__init__(self, self.wait_for_ping)
+                self.add_event_transitions('ping', {
+                    self.wait_for_ping:  self.wait_for_ping
+                })
 
-                def __init__(self, name):
-                    self.name = name
-                    StateMachine.__init__(self, self.wait_for_ping)
-                    self.add_event_transitions('ping', {
-                        self.wait_for_ping:  self.wait_for_ping
-                    })
+        a = A('a')
+        b = B('b')
+        a.send('ping', b)
+        MACHINE_SET.process_all_events()
 
-            a = A('a')
-            b = B('b')
-            a.send('ping', b)
-            MACHINE_SET.process_all_events()
+        test.assertEqual(sequence_diagram['a'], ['wait_for_pong', 'wait_for_ping'])
 
-            test.assertEqual(sequence_diagram['a'], ['wait_for_pong', 'wait_for_ping'])
 
+
+    def test_bad_initial_state(test):
+        def foo():
+            pass
+
+        class Z(StateMachine):
+            def set_state(self, new_state):
+                pass
+
+            def bar(self):
+                pass
+
+            def __init__(self):
+                StateMachine.__init__(self, foo)
+
+        with test.assertRaises(AttributeError):
+            Z()
+
+
+    def test_bad_new_state(test):
+        def foo():
+            pass
+
+        class Z(StateMachine):
+            def set_state(self, new_state):
+                pass
+
+            def bar(self):
+                pass
+
+            def __init__(self):
+                StateMachine.__init__(self, self.bar)
+
+        z = Z()
+        with test.assertRaises(AttributeError):
+            z.add_event_transitions('some_event', {
+                z.bar: foo
+            })
+
+    def test_bad_current_state_in_transition(test):
+        def foo():
+            pass
+
+        class Z(StateMachine):
+            def set_state(self, new_state):
+                pass
+
+            def bar(self):
+                pass
+
+            def __init__(self):
+                StateMachine.__init__(self, self.bar)
+
+        z = Z()
+        with test.assertRaises(AttributeError):
+            z.add_event_transitions('some_event', {
+                foo: z.bar
+            })
+
+
+    def test_bad_event(test):
+        class W(StateMachine):
+
+            def __init__(self):
+                StateMachine.__init__(self, None)
+                self.add_event_transitions('honk', { None: None })
+
+        w = W()
+
+        with test.assertRaises(ValueError):
+            w.send('bogus_event')
