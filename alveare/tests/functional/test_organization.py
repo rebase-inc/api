dfeaaa88d3362173c322e2ffc48488aea572fff0
Raphael Goyran <rapha.open.source@gmail.com>
2015-03-03 11:16:33 -0800

    Add DELETE to Organization resource, with test

--- alveare/tests/functional/test_organization.py
+++ alveare/tests/functional/test_organization.py
@@ -1,6 +1,6 @@
 from . import AlveareRestTestCase
 
-res_url = 'organizations/{}'.format
+url = 'organizations/{}'.format
 
 class TestOrganizationResource(AlveareRestTestCase):
 
@@ -17,9 +17,20 @@ class TestOrganizationResource(AlveareRestTestCase):
 
         org_0 = orgs[0]
 
-        org_response = self.get_resource(res_url(org_0['id']))
+        org_response = self.get_resource(url(org_0['id']))
         self.assertIn('organization', org_response)
         org = org_response['organization']
 
         self.assertEqual(org['name'], 'Alveare')
 
+    def test_delete_one(self):
+        response = self.get_resource('organizations')
+        self.assertIn('organizations', response)
+        orgs = response['organizations']
+        self.assertGreater(len(orgs), 1)
+
+        org_0 = orgs[0]
+
+        self.delete_resource(url(org_0['id']))
+
+        self.get_resource(url(org_0['id']), 404)
--- alveare/views/organization.py
+++ alveare/views/organization.py
@@ -5,6 +5,7 @@ class Organization(Schema):
     id = fields.Integer()
     name = fields.String()
     #projects = fields.Nested(Project, many=True)
+    #bank_accounts = fields.Nested(BankAccount, many=True)
 
 deserializer = serializer = Organization()
 
