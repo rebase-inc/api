79df89ca07b53bfaf185db7caac81a7dc550ad11
Raphael Goyran <rapha.open.source@gmail.com>
2015-03-05 17:50:10 -0800

    Add more functional tests for bank accounts

--- alveare/tests/functional/test_bank_account.py
+++ alveare/tests/functional/test_bank_account.py
@@ -4,6 +4,15 @@ from unittest import skip
 
 url = 'bank_accounts/{}'.format
 
+def name(res):
+    if 'user' in res.keys():
+        user = res['user']
+        return user['first_name']+' '+user['last_name']
+    elif 'projects' in res.keys():
+        return res['name']
+    else:
+        raise TypeError('res should be Contractor or Organization')
+
 class TestBankAccountResource(AlveareRestTestCase):
 
     def test_get_all(self):
@@ -24,38 +33,59 @@ class TestBankAccountResource(AlveareRestTestCase):
         self.assertEqual(account['account_number'], account_data['account_number'])
         return account
 
-    def find_org_with_no_bank_account(self):
-        orgs = self.get_resource('organizations')['organizations']
-        self.assertTrue(orgs)
-        no_account_orgs = list(filter(lambda org: org['bank_account']==0, orgs))
-        self.assertTrue(no_account_orgs)
-        return no_account_orgs[0]
+    def find_resource_with_no_bank_account(self, resources):
+        ''' resources can either 'organizations' or 'contractors' '''
+        response = self.get_resource(resources)
+        self.assertIn(resources, response)
+        all_resources = response[resources]
+        resources_with_no_bank_account = list(filter(lambda org: org['bank_account']==0, all_resources))
+        self.assertTrue(resources_with_no_bank_account)
+        return resources_with_no_bank_account[0]
 
-    def find_contractor_with_no_bank_account(self):
-        contractors = self.get_resource('contractors')['contractors']
-        self.assertTrue(contractors)
-        no_account_contractors = list(filter(lambda c: c['bank_account']==0, contractors))
-        self.assertTrue(no_account_contractors)
-        return no_account_contractors[0]
-
-    def get_org(self, org_id):
-        return self.get_resource('organizations/{}'.format(org_id))['organization']
-
-    def get_contractor(self, contractor_id):
-        return self.get_resource('contractors/{}'.format(contractor_id))['contractor']
+    def get(self, resource, resource_id):
+        response = self.get_resource('{}s/{}'.format(resource, resource_id))
+        self.assertIn(resource, response)
+        return response[resource]
 
     def test_create_org_account(self):
-        org = self.find_org_with_no_bank_account()
+        org = self.find_resource_with_no_bank_account('organizations')
         account = self.create_bank_account('organization_id', org['id'], org['name'])
 
-        updated_org = self.get_org(org['id'])
+        updated_org = self.get('organization', org['id'])
         self.assertEqual(updated_org['bank_account'], account['id'])
 
-    #@skip('TODO: implement the Contractor resource')
-    def test_create_contractor_account(self):
-        contractor = self.find_contractor_with_no_bank_account()
-        account = self.create_bank_account('contractor_id', contractor['id'], contractor['user']['first_name']+' '+contractor['user']['last_name'])
+    def test_contractor_account(self):
+        contractor = self.find_resource_with_no_bank_account('contractors')
+        account = self.create_bank_account('contractor_id', contractor['id'], name(contractor))
 
-        updated_contractor = self.get_contractor(contractor['id'])
+        updated_contractor = self.get('contractor', contractor['id'])
         self.assertEqual(updated_contractor['bank_account'], account['id'])
 
+    def delete_bank_account(self, resource, name_fn):
+        '''
+        resource can be 'organization' or 'contractor'
+        name_fn is a function that, given an instance of a resource, returns a name (str)
+        '''
+        owner = self.find_resource_with_no_bank_account('{}s'.format(resource))
+        account = self.create_bank_account('{}_id'.format(resource), owner['id'], name_fn(owner))
+        account_url = url(account['id'])
+        self.delete_resource(account_url)
+        self.get_resource(account_url, 404)
+        same_owner = self.get(resource, owner['id'])
+        self.assertEqual(same_owner['bank_account'], 0)
+
+
+    def test_delete_contractor_bank_account(self):
+        self.delete_bank_account('contractor', name)
+
+    def test_delete_organization_bank_account(self):
+        self.delete_bank_account('organization', name)
+
+    def test_delete_organization(self):
+        contractor = self.find_resource_with_no_bank_account('contractors')
+        account = self.create_bank_account('contractor_id', contractor['id'], name(contractor))
+        account_url = url(account['id'])
+        self.delete_resource(account_url)
+        self.get_resource(account_url, 404)
+        same_contractor = self.get('contractor', contractor['id'])
+        self.assertEqual(same_contractor['bank_account'], 0)
