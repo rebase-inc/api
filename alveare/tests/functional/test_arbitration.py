bd66603d56fdbb274bc7da603bf219c95c2f592d
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2015-04-01 14:21:30 -0700

    Add permission and tests for arbitration

--- alveare/tests/functional/test_arbitration.py
+++ alveare/tests/functional/test_arbitration.py
@@ -5,6 +5,7 @@ import datetime
 import unittest
 
 from . import AlveareRestTestCase
+from alveare.models import Arbitration
 
 class TestArbitrationResource(AlveareRestTestCase):
 
@@ -43,4 +44,41 @@ class TestArbitrationResource(AlveareRestTestCase):
         self.login_admin()
         ''' admin only '''
         pass
+
+    def test_contractor_can_get_their_own(self):
+        arbitration = Arbitration.query.first()
+        contractor = arbitration.mediation.work.offer.contractor
+        self.get_resource('arbitrations/{}'.format(arbitration.id), 401)
+        self.login(contractor.user.email, 'foo')
+        self.get_resource('arbitrations/{}'.format(arbitration.id))
+
+        returned_arbitrations = self.get_resource('arbitrations')['arbitrations']
+        returned_arbitration_ids = [a['id'] for a in returned_arbitrations]
+        actual_arbitration_ids = []
+        for work_offer in contractor.work_offers:
+            if work_offer.work and work_offer.work.mediation_rounds:
+                for mediation_round in work_offer.work.mediation_rounds:
+                    if mediation_round.arbitration:
+                        actual_arbitration_ids.append(mediation_round.arbitration.id)
+        self.assertEqual(set(returned_arbitration_ids), set(actual_arbitration_ids))
+
+        self.login_as_new_user()
+        arbitrations = self.get_resource('arbitrations')['arbitrations']
+        arbitration_ids = [a['id'] for a in arbitrations]
+        self.assertNotIn(arbitration.id, arbitration_ids)
+
+    def test_manager_of_auction_can_see_them(self):
+        arbitration = Arbitration.query.first()
+        manager = arbitration.mediation.work.offer.bid.auction.organization.managers[0]
+        self.login(manager.user.email, 'foo')
+        self.get_resource('arbitrations/{}'.format(arbitration.id))
+        arbitrations = self.get_resource('arbitrations')['arbitrations']
+        arbitration_ids = [a['id'] for a in arbitrations]
+        self.assertIn(arbitration.id, arbitration_ids)
+
+        self.login_as_new_user()
+        self.get_resource('arbitrations/{}'.format(arbitration.id), 401)
+        arbitrations = self.get_resource('arbitrations')['arbitrations']
+        arbitration_ids = [a['id'] for a in arbitrations]
+        self.assertNotIn(arbitration.id, arbitration_ids)
 
