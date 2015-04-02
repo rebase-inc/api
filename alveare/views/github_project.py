fd92337b457e28d1bb14d775269ac9e55c0e35e4
Raphael Goyran <rapha.open.source@gmail.com>
2015-04-02 11:16:04 -0700

    Layout cleanup (not code change) for project and github_project schemas

--- alveare/views/github_project.py
+++ alveare/views/github_project.py
@@ -1,24 +1,22 @@
 from marshmallow import fields
 from alveare.common.schema import AlveareSchema
-from alveare.models.organization import Organization
 from alveare.models.github_project import GithubProject
-from alveare.models.code_repository import CodeRepository
 from alveare.common.database import get_or_make_object, SecureNestedField
 
 class GithubProjectSchema(AlveareSchema):
 
-    id =              fields.Integer()
-    name =            fields.String()
-    organization =    SecureNestedField('OrganizationSchema', only=('id',), required=True)
-    code_repository = SecureNestedField('CodeRepositorySchema', only=('id',))
-    tickets =         SecureNestedField('TicketSchema', only=('id',))
-    clearances =      SecureNestedField('CodeClearanceSchema', only=('id',))
+    id =                fields.Integer()
+    name =              fields.String()
+    organization =      SecureNestedField('OrganizationSchema',     only=('id',), allow_null=True)
+    clearances =        SecureNestedField('CodeClearanceSchema',    only=('id',), many=True)
+    tickets =           SecureNestedField('TicketSchema',           only=('id',), many=True)
+    code_repository =   SecureNestedField('CodeRepositorySchema',   only=('id',))
+
 
     def make_object(self, data):
-        from alveare.models import GithubProject
         return get_or_make_object(GithubProject, data)
 
 serializer =            GithubProjectSchema(only=('id', 'organization', 'name'), skip_missing=True)
 deserializer =          GithubProjectSchema(only=('organization', 'name'), strict=True)
-update_deserializer =   GithubProjectSchema(only=tuple())
+update_deserializer =   GithubProjectSchema()
 update_deserializer.make_object = lambda data: data
--- alveare/views/project.py
+++ alveare/views/project.py
@@ -1,27 +1,23 @@
 from marshmallow import fields
 from alveare.common.schema import AlveareSchema
-from alveare.common.database import DB
 from alveare.models.project import Project
-from alveare.models.user import User
-from alveare.models.organization import Organization
 from alveare.common.database import get_or_make_object, SecureNestedField
-from alveare.views.ticket import TicketSchema
 
 class ProjectSchema(AlveareSchema):
+
     id =                fields.Integer()
     name =              fields.String()
+    organization =      SecureNestedField('OrganizationSchema',     only=('id',), allow_null=True)
+    clearances =        SecureNestedField('CodeClearanceSchema',    only=('id',), many=True)
+    tickets =           SecureNestedField('TicketSchema',           only=('id',), many=True)
+    code_repository =   SecureNestedField('CodeRepositorySchema',   only=('id',))
     type =              fields.String()
-    organization =      SecureNestedField('OrganizationSchema', only=('id',), allow_null=True)
-    clearances =        SecureNestedField('CodeClearanceSchema', only=('id',), many=True)
-    tickets =           SecureNestedField('TicketSchema', only=('id',), many=True)
-    code_repository =   SecureNestedField('CodeRepositorySchema', only=('id',))
 
     def make_object(self, data):
-        from alveare.models import Project
         return get_or_make_object(Project, data)
 
-serializer = ProjectSchema(skip_missing=True)
-deserializer = ProjectSchema(only=('organization', 'name'))
-update_deserializer = ProjectSchema()
+serializer =            ProjectSchema(skip_missing=True)
+deserializer =          ProjectSchema(only=('organization', 'name'))
+update_deserializer =   ProjectSchema()
 update_deserializer.make_object = lambda data: data
 
