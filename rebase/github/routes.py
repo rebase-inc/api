67fae48f9893b5fbc9b5e814d41697ed6e9d3185
Raphael A. Goyran <raphael@alveare.io>
2015-08-06 17:10:42 -0700

    Pull the repos for a logged in GitHub user

--- rebase/github/routes.py
+++ rebase/github/routes.py
@@ -7,7 +7,7 @@ def register_github_routes(app):
         'github',
         consumer_key='ccfe7b7be7560c9a112e',
         consumer_secret='1779c1d363dec567c81c01ef266e4d3f30f79a8d',
-        request_token_params={'scope': 'user:email'},
+        request_token_params={'scope': 'user, repo'},
         base_url='https://api.github.com/',
         request_token_url=None,
         access_token_method='POST',
@@ -34,15 +34,24 @@ def register_github_routes(app):
     @app.route('/github/authorized')
     def authorized():
         resp = github.authorized_response()
-        app.logger.debug(resp)
         if resp is None:
             return 'Access denied: reason=%s error=%s' % (
                 request.args['error'],
                 request.args['error_description']
             )
+        app.logger.debug(resp)
         session['github_token'] = (resp['access_token'], '')
-        me = github.get('user')
-        return jsonify(me.data)
+        me = github.get('user/emails')
+        app.logger.debug(me.data)
+        return jsonify({'result': 'complete success'})
+
+    @app.route('/github/repos')
+    def repos():
+        if 'github_token' in session:
+            me = github.get('user/repos')
+            app.logger.debug(me.data)
+            return jsonify({'repos': me.data})
+        return redirect(url_for('login'))
 
     @github.tokengetter
     def get_github_oauth_token():
