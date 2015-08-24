4240eb776803cfcc9143aefb13281d786eb65e66
Andrew Millspaugh <andrew@alveare.io>
2015-08-24 15:04:08 -0700

    Removed print debug statement

--- rebase/resources/auth.py
+++ rebase/resources/auth.py
@@ -24,7 +24,6 @@ class AuthCollection(Resource):
 
     # TODO: Refactor this to look like other REST endpoints
     def post(self):
-        print(request.form or request.get_json())
         try:
             auth_data = auth.deserializer.load(request.form or request.json).data
             user = auth_data['user']
