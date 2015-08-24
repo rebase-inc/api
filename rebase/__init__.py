268bc71c85d39475f900d027c0595f030ae26f67
Andrew Millspaugh <andrew@alveare.io>
2015-08-24 13:35:44 -0700

    Fixed reference error

--- rebase/__init__.py
+++ rebase/__init__.py
@@ -1,7 +1,7 @@
 import sys
 from os import environ
 
-from flask import Flask
+from flask import Flask, request
 from flask.ext.restful import Api
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.engine import Engine
@@ -42,10 +42,10 @@ def create_app(testing=False):
     def add_cors(resp):
         """ Ensure all responses have the CORS headers. This ensures any failures are also accessible
         by the client. """
-        resp.headers['Access-Control-Allow-Origin'] = flask.request.headers.get('Origin','*')
+        resp.headers['Access-Control-Allow-Origin'] = request.headers.get('Origin','*')
         resp.headers['Access-Control-Allow-Credentials'] = 'true'
         resp.headers['Access-Control-Allow-Methods'] = 'PUT', 'POST, OPTIONS, GET'
-        resp.headers['Access-Control-Allow-Headers'] = flask.request.headers.get('Access-Control-Request-Headers', 'Authorization' )
+        resp.headers['Access-Control-Allow-Headers'] = request.headers.get('Access-Control-Request-Headers', 'Authorization' )
         # set low for debugging
         if app.debug:
             resp.headers['Access-Control-Max-Age'] = '1000'
