0dc0f98ce77bec8479a47987757e85c11c3616c4
Andrew Millspaugh <andrew@alveare.io>
2015-08-24 14:33:38 -0700

    Added debug print statement because I cant figure out why login isnt working

--- rebase/resources/auth.py
+++ rebase/resources/auth.py
@@ -24,6 +24,7 @@ class AuthCollection(Resource):
 
     # TODO: Refactor this to look like other REST endpoints
     def post(self):
+        print(request.form or request.json)
         try:
             auth_data = auth.deserializer.load(request.form or request.json).data
             user = auth_data['user']
