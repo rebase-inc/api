524b9e36e23d9f957f9a1d9c07a0f5952fdb2bbe
Andrew Millspaugh <andrew@alveare.io>
2015-08-25 13:14:26 -0700

    Add bid field to contract serializer

--- rebase/views/contract.py
+++ rebase/views/contract.py
@@ -6,12 +6,12 @@ from rebase.common.database import get_or_make_object, SecureNestedField
 
 class ContractSchema(RebaseSchema):
     id =  fields.Integer()
-    bid = SecureNestedField('BidSchema', only=('id',))
+    bid = SecureNestedField('BidSchema', only=('id','work_offers'))
 
     def make_object(self, data):
         from rebase.models import Contract
         return get_or_make_object(Contract, data)
 
-serializer = ContractSchema(only=('id',), skip_missing=True)
+serializer = ContractSchema(only=('id','bid'), skip_missing=True)
 deserializer = ContractSchema(only=('bid',), strict=True)
 update_deserializer = ContractSchema()
