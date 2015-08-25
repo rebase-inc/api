3b5fdda5754d4709a06dc427384d148ead194014
Andrew Millspaugh <andrew@alveare.io>
2015-08-24 20:33:14 -0700

    more debugging statements

--- rebase/common/mock.py
+++ rebase/common/mock.py
@@ -177,14 +177,17 @@ def create_one_auction(db, tickets=None, duration=1000, finish_work_by=None, red
     from rebase.models import Auction, TicketSet, BidLimit, TicketSnapshot, TermSheet
     tickets = tickets or create_some_tickets(db)
     ticket_snaps = [TicketSnapshot(ticket) for ticket in tickets]
-    print('we have the ticket snaps of ')
-    print(ticket_snaps)
     bid_limits = [BidLimit(ticket_snap, 200) for ticket_snap in ticket_snaps]
     term_sheet = TermSheet('Some legal mumbo-jumbo')
     ticket_set = TicketSet(bid_limits)
     organization = tickets[0].project.organization
     auction = Auction(ticket_set, term_sheet, duration, finish_work_by, redundancy)
     db.session.add(auction)
+    print('we got these things:')
+    print(tickets)
+    print(ticket_snaps)
+    print(bid_limits)
+    print(ticket_set)
     return auction
 
 def create_one_nomination(db, auction=None, contractor=None, approved=False):
@@ -292,6 +295,7 @@ class UserStory(object):
         self.password = password
 
     def create(self, db):
+        print('Making the user story!')
         if self.type == self.types['NEW_DEVELOPER']:
             self.user = create_one_user(db, self.first_name, self.last_name, self.email, self.password)
             user_ted = create_one_user(db, 'Ted', 'Crisp', 'tedcrisp@joinrebase.com')
