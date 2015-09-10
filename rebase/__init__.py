1f15530b092b9ca57c90c96652d66de217455891
Raphael A. Goyran <raphael@alveare.io>
2015-09-10 16:07:33 -0700

    Fix for SQLAlchemy DB object configuration.
    Calling DB.init_app(app) will reset some configuration parameters
    on the app object, thereby nullifying some settings applied when
    calling app.config.from_object().

--- rebase/__init__.py
+++ rebase/__init__.py
@@ -4,9 +4,6 @@ from os import environ, urandom
 from flask import Flask, request
 from flask.ext.restful import Api
 from flask_debugtoolbar import DebugToolbarExtension
-from sqlalchemy.engine import Engine
-from sqlalchemy import event
-from flask.ext.sqlalchemy import SQLAlchemy
 
 from rebase.common.routes import register_routes
 from rebase.common.database import DB, DB_PRODUCTION_NAME
@@ -24,16 +21,16 @@ def create_app(testing=False):
     if 'DATABASE_URL' not in environ or 'APP_SETTINGS' not in environ:
         raise EnvironmentError('Missing environment variables. Did you forget to run "source setup.sh" or "source test_setup.sh"?')
     app = Flask(__name__)
-    app.config.from_object(environ['APP_SETTINGS'])
     app_context = app.app_context()
     app_context.push()
+    DB.init_app(app)
+    app.config.from_object(environ['APP_SETTINGS'])
     toolbar = DebugToolbarExtension(app)
     app.config['SQLALCHEMY_DATABASE_URI'] = environ['TEST_URL'] if testing else environ['DATABASE_URL']
     setup_admin(app, DB.session)
     setup_login(app)
     setup_rq(app)
     setup_cache(app)
-    DB.init_app(app)
     api = Api(app)
     register_home(app)
     register_routes(api)
