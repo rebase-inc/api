141b3f5c48fe4cdf525571b1cffe018db16a00fa
Raphael A. Goyran <raphael@alveare.io>
2015-09-14 13:10:40 -0700

    Cleanup test database creation logic

---
+++ rebase/tests/database.py
@@ -0,0 +1,35 @@
+import atexit
+from os import getpid, environ
+from subprocess import check_output, check_call, call
+
+
+TEST_DATABASE_PREFIX='rebase_test'
+DB_URL_PREFIX='postgresql://localhost'
+
+def all_databases():
+    output = check_output(['psql', '-lqt']).decode(encoding='UTF-8').splitlines()
+    return map(lambda line: line.lstrip().partition(' ')[0], output)
+
+def all_test_databases():
+    return filter(lambda database_name: database_name.startswith(TEST_DATABASE_PREFIX), all_databases())
+
+def exists(db_name):
+    return any(map(lambda database_name: database_name.startswith(db_name), all_databases()))
+
+@atexit.register
+def delete_all_test_databases():
+    for test_db in all_test_databases():
+        call(['dropdb', test_db])
+
+def make_one_test_database_for_this_process():
+    db_name = '{}_{}'.format(TEST_DATABASE_PREFIX, getpid())
+    test_url='{}/{}'.format(DB_URL_PREFIX, db_name)
+    if not exists(db_name):
+        #print(db_name+' does not exists, let\'s create it.')
+        check_call(['createdb', db_name])
+        environ['TEST_URL'] = test_url
+    else:
+        #print(db_name+' already exists.')
+        pass
+
+make_one_test_database_for_this_process()
