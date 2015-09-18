9103f65b33b386761e10d49c3847eaff625aff48
Raphael A. Goyran <raphael@alveare.io>
2015-09-18 15:07:05 -0700

    Revert GitHubTicket model to prevent hundreds of functtests from failing

--- rebase/models/github_ticket.py
+++ rebase/models/github_ticket.py
@@ -10,7 +10,7 @@ class GithubTicket(RemoteTicket):
 
     __mapper_args__ = { 'polymorphic_identity': 'github_ticket' }
 
-    def __init__(self, project, url):
+    def __init__(self, project, number, title='NOTIMPLEMENTED', description='NOTIMPLEMENTED'):
         self.project = project
         self.number = number
         self.title = title #this should be pulled from github
