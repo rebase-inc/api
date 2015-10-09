604167fa7ade4880ab7c2aedc007928b4988b3d9
Raphael A. Goyran <raphael@alveare.io>
2015-10-09 15:59:37 -0700

    Refactor the github project import code a bit

--- rebase/github/scanners.py
+++ rebase/github/scanners.py
@@ -16,7 +16,15 @@ from rebase.models import (
 )
 from rebase.views.organization import serializer as org_serializer
 from rebase.views.project import serializer as project_serializer
+from rebase.views.manager import serializer as mgr_serializer
 from rebase.views.code_repository import serializer as repo_serializer
+
+_serializers = {
+    'repos':    repo_serializer,
+    'projects': project_serializer,
+    'orgs':     org_serializer,
+    'managers': mgr_serializer
+}
 
 GithubSession = namedtuple('GithubSession', ['api', 'account', 'user', 'DB'])
 
@@ -75,11 +83,9 @@ def import_github_repos(repos, user, db_session):
     '''
         Imports the 'repos' and returns an object with the created or updated CodeRepository, Project and Organization instances
     '''
-    new_data = {
-        'repos':        [],
-        'projects':    [],
-        'orgs':         [],
-    }
+    # in new_data we will store the newly created object, e.g. Managers, Organizations, GithubProjects, etc.
+    new_data = { k: [] for k in _serializers.keys() }
+    print(new_data)
     orgs = {}
     for repo_id, repo in repos.items():
         _org = repo['org']
@@ -111,13 +117,11 @@ def import_github_repos(repos, user, db_session):
             mgr = Manager(user, _repo.project)
             _repo.project.managers.append(mgr)
             db_session.add(_repo)
+            new_data['managers'].append(mgr)
         new_data['projects'].append(_repo.project)
         new_data['repos'].append(_repo)
 
     db_session.commit()
 
     # serialize the new data
-    new_data['projects'] = list(map(lambda prj: project_serializer.dump(prj).data, new_data['projects']))
-    new_data['orgs'] = list(map(lambda org: org_serializer.dump(org).data, new_data['orgs']))
-    new_data['repos'] = list(map(lambda repo: repo_serializer.dump(repo).data, new_data['repos']))
-    return new_data
+    return { key: list(map(lambda elt: _serializers[key].dump(elt).data, value)) for key, value in new_data.items() }
