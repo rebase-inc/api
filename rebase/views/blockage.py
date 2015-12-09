94ca45f9fd9a12b620484a984de8d77396efbe07
Raphael A. Goyran <raphael@alveare.io>
2015-12-08 16:23:07 -0800

    Create a Blockage model with its own Comments

---
+++ rebase/views/blockage.py
@@ -0,0 +1,19 @@
+
+from marshmallow import fields, post_load
+
+from rebase.common.schema import RebaseSchema
+from rebase.common.database import SecureNestedField
+
+class BlockageSchema(RebaseSchema):
+    id =            fields.Integer()
+    comments =      SecureNestedField('CommentSchema', only=('id', 'content', 'created', 'user', 'type'), many=True, default=None)
+
+    @post_load
+    def make_blockage(self, data):
+        from rebase.models import Blockage
+        return self._get_or_make_object(Blockage, data)
+
+
+serializer =            BlockageSchema()
+deserializer =          BlockageSchema(strict=True)
+update_deserializer =   BlockageSchema(only=('id', ), context={'raw': True})
