2b9d1eee378773d93c82f041473db4d32b810db7
Raphael A. Goyran <raphael@alveare.io>
2015-12-09 14:54:47 -0800

    Update views to display debits/credits data

--- rebase/views/credit.py
+++ rebase/views/credit.py
@@ -7,14 +7,14 @@ class CreditSchema(RebaseSchema):
     id = fields.Integer()
     price = fields.Integer()
     paid = fields.Boolean()
-    work = SecureNestedField('WorkSchema', only='id')
+    work = SecureNestedField('WorkSchema')
 
     @post_load
     def make_credit(self, data):
         from rebase.models import Credit
         return self._get_or_make_object(Credit, data)
 
-serializer = CreditSchema(only=('id','work','price','paid'))
+serializer = CreditSchema()
 deserializer = CreditSchema(only=('work','price'))
 
 update_deserializer = CreditSchema(only=tuple(), context={'raw': True}, strict=True)
--- rebase/views/debit.py
+++ rebase/views/debit.py
@@ -7,14 +7,14 @@ class DebitSchema(RebaseSchema):
     id = fields.Integer()
     price = fields.Integer()
     paid = fields.Boolean()
-    work = SecureNestedField('WorkSchema', only='id')
+    work = SecureNestedField('WorkSchema')
 
     @post_load
     def make_debit(self, data):
         from rebase.models import Debit
         return self._get_or_make_object(Debit, data)
 
-serializer = DebitSchema(only=('id','work','price','paid'))
+serializer = DebitSchema()
 deserializer = DebitSchema(only=('work','price'))
 
 update_deserializer = DebitSchema(only=tuple(), context={'raw': True}, strict=True)
