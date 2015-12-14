8f7d95a57028f12f05f08b710908b567d86e5334
Raphael A. Goyran <raphael@alveare.io>
2015-12-14 13:22:24 -0800

    Enable static assets serving

--- rebase/__init__.py
+++ rebase/__init__.py
@@ -21,7 +21,7 @@ def create_app(testing=False):
     '''
     if 'DATABASE_URL' not in environ or 'APP_SETTINGS' not in environ:
         raise EnvironmentError('Missing environment variables. Did you forget to run "source setup.sh" or "source test_setup.sh"?')
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='')
     app_context = app.app_context()
     app_context.push()
     app.config.from_object(environ['APP_SETTINGS'])
--- rebase/home/routes.py
+++ rebase/home/routes.py
@@ -1,30 +1,8 @@
 import os
 
-from flask import render_template, session, send_from_directory
-from flask.ext.login import current_user
-
-from rebase.models.user import User
-from rebase.models.skill_set import SkillSet
-from rebase.models.contractor import Contractor
+from flask import send_from_directory
 
 def register_home(app):
     @app.route('/')
     def login_root():
-        if current_user.is_authenticated():
-            skill_set = SkillSet.query.join(Contractor).filter(Contractor.user==current_user).first()
-            languages = None
-            if skill_set:
-                languages = skill_set.skills
-            return render_template(
-                'main.html',
-                first = current_user.first_name,
-                last=current_user.last_name,
-                email=current_user.email,
-                languages=languages
-            )
-        else:
-            return render_template('login.html')
-
-    @app.route('/app/<path:path>')
-    def send_app(path):
-        return send_from_directory(os.path.abspath('../react-app/'), path, cache_timeout=None)
+        return send_from_directory(os.path.abspath('../react-app/'), 'index.html', cache_timeout=None)
