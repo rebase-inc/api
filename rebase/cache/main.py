945412817a08b890ddb353d352c3e81bd05220de
Raphael A. Goyran <raphael@alveare.io>
2016-01-20 16:55:44 -0800

    Clean up imports after rebase/cache re-org

--- rebase/cache/main.py
+++ rebase/cache/main.py
@@ -1,11 +1,17 @@
 from queue import Queue, Empty
 
+from rebase.cache.app import create
 from rebase.cache.tasks import warmup
+from rebase.models import (
+    Role,
+)
 
 def cache_main(user_role_id, q, name):
     app, _, db = create()
-    for user in User.query.all():
-        print('User: {}'.format(user))
+    role = Role.query.get(user_role_id[1])
+    if not role:
+        print('Unknown role, terminating')
+        return 1
     while True:
         try:
             task = q.get(timeout=600)
@@ -17,4 +23,4 @@ def cache_main(user_role_id, q, name):
             print('{} QUIT'.format(name, *user_role_id))
             break
         if task['action'] == 'warmup':
-            warmup(app, db, user_role_id)
+            warmup(app, db, role)
