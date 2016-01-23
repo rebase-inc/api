f07def3d3d3e8e8097d6d48f1fad9e2c120f99fd
Raphael A. Goyran <raphael@alveare.io>
2016-01-22 16:29:31 -0800

    Add role_id param to all cache tasks

--- rebase/cache/main.py
+++ rebase/cache/main.py
@@ -1,9 +1,8 @@
 from logging import info, debug, error
 from queue import Queue, Empty
 
-
 from rebase.cache.app import create
-from rebase.cache.role import init_role
+
 
 def cache_main(role_id, q, name):
     info('Started child process')
@@ -12,7 +11,6 @@ def cache_main(role_id, q, name):
     # which is needed wherever 'current_user' is read
     ctx = app.test_request_context('/foobar')
     ctx.push()
-    init_role(role_id)
     while True:
         try:
             task = q.get(timeout=3600)
@@ -21,5 +19,5 @@ def cache_main(role_id, q, name):
             break
         debug('Received: {}'.format(task))
         function, args, kwargs = task['action']
-        function(*args, **kwargs)
+        function(role_id, *args, **kwargs)
     info('Exiting child process')
--- rebase/cache/tasks.py
+++ rebase/cache/tasks.py
@@ -1,25 +1,36 @@
 from functools import partial
-from logging import info, debug
+from logging import info, debug, error
 from sys import exit
 
+from flask.ext.login import login_user
 
 from rebase.common.database import DB
 from rebase.common.stopwatch import Elapsed
+from rebase.models import Role
 
+def get_role(role_id):
+    info('Loading Role(%d)', role_id)
+    role = Role.query.get(role_id)
+    if not role:
+        error('Unknown role, terminating')
+        return
+    role.user.set_role(role.id)
+    login_user(role.user)
+    return role
 
-def warmup():
-    from rebase.cache.role import ROLE
+def warmup(role_id):
     from rebase.memoize import cache
     from rebase.resources.auction import get_all_auctions
-    cache.delete_memoized(get_all_auctions, ROLE.id)
-    with Elapsed(partial(info, 'warmup: running get_all_auctions for {} took %f seconds'.format(ROLE.id))):
-        get_all_auctions(ROLE.id)
+    role = get_role(role_id)
+    cache.delete_memoized(get_all_auctions, role.id)
+    with Elapsed(partial(info, 'warmup: running get_all_auctions for {} took %f seconds'.format(role.id))):
+        get_all_auctions(role.id)
 
-def cooldown():
+def cooldown(role_id):
     info('QUIT')
     exit(0)
 
-def invalidate(changeset):
+def invalidate(role_id, changeset):
     '''
     invalidate updates the list of potentially modified model instances 
     contained in 'changeset' and re-computes the cached functions
@@ -45,4 +56,4 @@ def invalidate(changeset):
     #debug('Current DB.identity_map:')
     #debug(tuple(DB.session.identity_map.keys()))
     info('Updating get_all_auctions cache after invalidation')
-    warmup()
+    warmup(role_id)
