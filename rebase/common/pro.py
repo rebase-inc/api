3e93e71576fc5b133a9b14c99fc32b5fd7407caf
Raphael Goyran <raphael@joinrebase.com>
2016-03-30 20:45:16 +0000

    Change email notification logins for both dev and pro.
    pro setup extracts password from environment.

--- rebase/common/pro.py
+++ rebase/common/pro.py
@@ -5,10 +5,11 @@ from rebase.common.env import check
 check([
     'SECRET_KEY',
     'GITHUB_CLIENT_SECRET',
+    'NOTIFICATION_EMAIL_PASSWORD',
 ])
 
-NOTIFICATION_EMAIL = 'com.rebaseapp.alpha@gmail.com'
-NOTIFICATION_EMAIL_PASSWORD = 'JgQ-b9q-en2-B7g'
+NOTIFICATION_EMAIL = 'com.rebaseapp.alpha'
+NOTIFICATION_EMAIL_PASSWORD = environ['NOTIFICATION_EMAIL_PASSWORD']
 FLASK_LOGIN_SESSION_PROTECTION = "strong" # WARNING: this will make Apache Bench fail login unless it is used to login as well
 SECRET_KEY = environ['SECRET_KEY']
 GITHUB_CLIENT_ID = '78a6f5326fe65be4fb21'
