0bc7238bdd958b849561948a280b1c58dc8e90ab
Raphael A. Goyran <rapha.open.source@gmail.com>
2016-04-27 21:18:38 -0700

    Add new code2resume container

--- rebase/home/routes.py
+++ rebase/home/routes.py
@@ -1,8 +1,14 @@
 import os
+from logging import getLogger
 
-from flask import send_from_directory
+from flask import send_from_directory, request
+
+logger = getLogger()
 
 def register_home(app):
     @app.route('/')
     def login_root():
+        # note this is only used in dev mode.
+        # in production, Nginx serves static content
+        logger.debug('Headers: %s', request.headers)
         return send_from_directory(os.path.abspath('../react-app/'), 'index.html', cache_timeout=None)
