74de67265bcf629f0252af27341896bc3971671f
Raphael A. Goyran <rapha.open.source@gmail.com>
2016-06-08 11:41:13 -0700

    Fix for scheduler after rebase.app.create return value change

--- scheduler.py
+++ scheduler.py
@@ -33,15 +33,15 @@ def load_new_events(sched):
             sched.cancel(event)
         auctions = Auction.query.filter(or_(Auction.state=='waiting_for_bids', Auction.state=='created')).all()
         for auction in auctions:
-            sched.enterabs(auction.expires, 0, auction_expired, argument=(auction.id,))
+            sched.enterabs(auction.expires, 0, auction_expired, argument=(app, auction.id,))
 
         works = Work.query.filter_by(state='in_progress').all()
         for work in works:
-            sched.enterabs(work.offer.bid.auction.finish_work_by, 0, work_expired, argument=(work.id,))
+            sched.enterabs(work.offer.bid.auction.finish_work_by, 0, work_expired, argument=(app, work.id,))
 
         mediations = Mediation.query.filter(or_(Mediation.state=='waiting_for_client', Mediation.state=='waiting_for_dev')).all()
         for mediation in mediations:
-            sched.enterabs(mediation.timeout, 0, mediation_timed_out, argument=(mediation.id,))
+            sched.enterabs(mediation.timeout, 0, mediation_timed_out, argument=(app, mediation.id,))
     logger.debug('Loaded %d Events in the queue', len(sched.queue))
 
 
@@ -69,31 +69,34 @@ def wait(sched, delta):
         sleep(delta.total_seconds())
 
 
-def auction_expired(auction_id):
-    auction = Auction.query.get(auction_id)
-    if auction and (auction.state == 'waiting_for_bids' or auction.state == 'created'):
-        with ManagedState():
-            auction.machine.send('fail')
-        DB.session.commit()
-        logger.info('%s has expired now', auction)
+def auction_expired(app, auction_id):
+    with app.app_context():
+        auction = Auction.query.get(auction_id)
+        if auction and (auction.state == 'waiting_for_bids' or auction.state == 'created'):
+            with ManagedState():
+                auction.machine.send('fail')
+            DB.session.commit()
+            logger.info('%s has expired now', auction)
 
 
-def work_expired(work_id):
-    work = Work.query.get(work_id)
-    if work and work.state == 'in_progress':
-        with ManagedState():
-            work.machine.send('review')
-        DB.session.commit()
-        logger.info('%s has expired now', work)
+def work_expired(app, work_id):
+    with app.app_context():
+        work = Work.query.get(work_id)
+        if work and work.state == 'in_progress':
+            with ManagedState():
+                work.machine.send('review')
+            DB.session.commit()
+            logger.info('%s has expired now', work)
 
 
-def mediation_timed_out(mediation_id):
-    mediation = Mediation.query.get(mediation_id)
-    if mediation and (mediation.state == 'waiting_for_client' or mediation.state == 'waiting_for_dev'):
-        with ManagedState():
-            mediation.machine.send('timeout')
-        DB.session.commit()
-        logger.info('%s has expired now', mediation)
+def mediation_timed_out(app, mediation_id):
+    with app.app_context():
+        mediation = Mediation.query.get(mediation_id)
+        if mediation and (mediation.state == 'waiting_for_client' or mediation.state == 'waiting_for_dev'):
+            with ManagedState():
+                mediation.machine.send('timeout')
+            DB.session.commit()
+            logger.info('%s has expired now', mediation)
 
 
 def quit(signal_number, frame):
