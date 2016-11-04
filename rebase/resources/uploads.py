2aab6cc8147a54a6c551cbef75160c05f03388b4
Raphael A. Goyran <rapha.open.source@gmail.com>
2016-11-03 17:52:46 -0700

    Move rebase/api docker image to rebase/dev-web
    Step 0 in the effort to get rid of locally
    checked out source code in production.
    
    Later will add rebase/pro-web which will
    not mount the source code directory into the container,
    allowing finally our backend to scale beyond one machine.

--- rebase/resources/uploads.py
+++ rebase/resources/uploads.py
@@ -1,5 +1,6 @@
-import magic
+from atexit import register
 import hashlib
+from magic import Magic, MAGIC_MIME_TYPE
 
 from os import path
 from flask.ext.restful import Resource
@@ -10,34 +11,37 @@ from rebase.views import user
 from rebase.models import Photo
 from rebase.common.database import DB
 
-ALLOWED_MIMETYPES = set(['image/jpeg'])
+
+ALLOWED_MIMETYPES = { 'image/jpeg' }
+MAGIC = Magic(flags=MAGIC_MIME_TYPE)
+register(MAGIC.close)
+
 
 def allowed_file_type(filebuffer):
-    with magic.Magic(flags=magic.MAGIC_MIME_TYPE) as m:
-        mimetype = m.id_buffer(filebuffer)
-        if mimetype in ALLOWED_MIMETYPES:
-            return True
-        else:
-            return False
+    return MAGIC.id_buffer(filebuffer) in ALLOWED_MIMETYPES
+
 
 class UploadCollection(Resource):
     url = '/uploads'
 
     def post(self):
         file = request.files['photo']
-        if allowed_file_type(file.stream.getvalue()):
-            filename = hashlib.sha1(file.stream.getvalue()).hexdigest() + '.jpeg'
-            file.save(path.join(current_app.config['UPLOAD_FOLDER'], filename))
-            photo = Photo.query.filter(Photo.user_id == current_user.id).first()
-            if photo:
-                photo.filename = filename
-            else:
-                photo = Photo(filename=filename, user=current_user)
-                DB.session.add(photo)
-            DB.session.commit()
-            response = jsonify(**{'user': user.serializer.dump(current_user).data, 'message': 'Photo Uploaded'})
-            response.status_code = 200
-            return response
+        # TODO uncomment the following line once Alpine Linux version 3.4.6 (upcoming) has been released.
+        # That release has a fix for: https://bugs.alpinelinux.org/issues/5264#change-17546
+        # which currently prevents Magic from working.
+        #if allowed_file_type(file.stream.getvalue()):
+        filename = hashlib.sha1(file.stream.getvalue()).hexdigest() + '.jpeg'
+        file.save(path.join(current_app.config['UPLOAD_FOLDER'], filename))
+        photo = Photo.query.filter(Photo.user_id == current_user.id).first()
+        if photo:
+            photo.filename = filename
+        else:
+            photo = Photo(filename=filename, user=current_user)
+            DB.session.add(photo)
+        DB.session.commit()
+        response = jsonify(**{'user': user.serializer.dump(current_user).data, 'message': 'Photo Uploaded'})
+        response.status_code = 200
+        return response
 
 class UploadResource(Resource):
     url = '/uploads/<filename>'
